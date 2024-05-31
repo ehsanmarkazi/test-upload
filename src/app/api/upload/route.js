c9ec35d51c1ede2f0c974f326bf162cacb6306dd
ehsnull <ehsanmarkazi73@gmail.com>
2024-05-31 22:51:13 +0330

    update

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,9 +1,7 @@
 import { NextResponse } from 'next/server';
 import fs from 'fs';
-import path from 'path';
 import { pipeline } from 'stream';
 import { promisify } from 'util';
-import { fileURLToPath } from 'url';
 
 const pump = promisify(pipeline);
 
@@ -12,25 +10,20 @@ const pump = promisify(pipeline);
 export async function POST(req) {
   try {
     const formData = await req.formData();
-    const file = formData.get('file'); // Assuming the input field name is 'file'
+    const file = formData.get('file'); 
+    const filePath = `./public/${file.name}`;
 
-    // Get the current directory
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
+    console.log(__dirname)
 
-    const uploadDir = path.join(__dirname, '../../../public/uploads'); // Adjust the path accordingly
-    const filePath = path.join(uploadDir, file.name);
 
-    console.log(uploadDir);
-
-    // Create the upload directory if it doesn't exist
+    const uploadDir = './public';
     if (!fs.existsSync(uploadDir)) {
       fs.mkdirSync(uploadDir, { recursive: true });
     }
 
     await pump(file.stream(), fs.createWriteStream(filePath));
 
-    const publicPath = `/uploads/${file.name}`;
+    const publicPath = `/${file.name}`;
     return NextResponse.json({ status: "success", data: publicPath });
   } catch (e) {
     console.error(e);
