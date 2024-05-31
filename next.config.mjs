53aa09472a3ac93a2ff6b36fa88222a2d106458d
ehsnull <ehsanmarkazi73@gmail.com>
2024-05-31 19:19:02 +0330

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+          "test-upload-theta-ten.vercel.app/upload",
+         
+          "localhost",
+  
+        ],
+      },
+      experimental: { esmExternals: true, serverActions: true },
+};
 
 export default nextConfig;
